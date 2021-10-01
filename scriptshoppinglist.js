6587ab2a20766a794768b74e824b5b09c422c02a
MiroslavDina <miroslav.dina86@gmail.com>
2021-10-01 12:15:40 +0200

    Update scriptshoppinglist.js
    huge changes

--- scriptshoppinglist.js
+++ scriptshoppinglist.js
@@ -2,9 +2,38 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
-	console.log("click is working");
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
 	var li = document.createElement("li");
-	li.appendChild(document.createTextNode("testing"));
-	ul.appendChild(li);
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		input.value = "";
+}
+
+
+button.addEventListener("click", function() {
+	// console.log("click is working");
+	// console.log(input.value);
+	if (inputLength() > 0) {
+		createListElement();
+		// var li = document.createElement("li");
+		// li.appendChild(document.createTextNode(input.value));
+		// ul.appendChild(li);
+		// input.value = "";
+	}
+})
+
+input.addEventListener("keypress", function(event) {
+	// console.log("event");
+	// console.log(input.value);
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+		// var li = document.createElement("li");
+		// li.appendChild(document.createTextNode(input.value));
+		// ul.appendChild(li);
+		// input.value = "";
+	}
 })
