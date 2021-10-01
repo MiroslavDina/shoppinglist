3167f9edfe3aae19b6d9091c6f34b2444f5f7171
MiroslavDina <miroslav.dina86@gmail.com>
2021-10-01 11:44:04 +0200

    Update scriptshoppinglist.js
    button, input, listener

--- scriptshoppinglist.js
+++ scriptshoppinglist.js
@@ -1,1 +1,6 @@
-var
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+
+button.addEventListener("click", function() {
+	console.log("click is working");
+})
